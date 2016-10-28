62deff6ccbc49042fdf709bf87a7bc3f31be48e6
lawrips <lawrence@ripsher.com>
2016-10-27 23:55:08 -0700

    storing first set of changes

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ var debug = require('debug')('whisper');
 var nconf = require('nconf');
 nconf.env().file({ file: './config/' + (process.env.NODE_ENV || 'production') + '.json' });
 var app = require('./lib/app');
+
